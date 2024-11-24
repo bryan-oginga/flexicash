ac4b30f0da24ee49419cfcaf11246839355d79e4
bryan-oginga <ogingabrian2017@gmail.com>
2024-11-24 23:23:22 +0300

    .

--- flexicash/urls.py
+++ flexicash/urls.py
@@ -1,6 +1,7 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +9,6 @@ urlpatterns = [
     path('dashboard/', include('reports.urls',namespace='reports')),  
     path('payment/', include('lipanampesa.urls',namespace='payment')),  
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
