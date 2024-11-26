446a9b1c96670fe034ae169a69dff64c58828d4d
bryan-oginga <ogingabrian2017@gmail.com>
2024-11-26 19:07:12 +0300

    .

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Making migrations..."
-python manage.py makemigrations
+python manage.py makemigrations loanapplication fleximembers transactions accounts lipanampesa 
 
 echo "Applying migrations..."
 python manage.py migrate
